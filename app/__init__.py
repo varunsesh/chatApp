f60b6316e1efcba9e8e6e7a2068f4ac93f4a2f66
Varun <varunsesh@gmail.com>
2021-12-08 11:14:24 +0530

    Refactored + modified some templates and package for app

--- app/__init__.py
+++ app/__init__.py
@@ -8,18 +8,10 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "it's-restricted"
-    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-    
-    db.init_app(app) ##Initiliase sql database
-    #Create login manager, lets code and flask login manager work together
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
 
-    from .auth import auth
+    from .view import view
 
-    app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(view, url_prefix="/")
 
     
     return app
